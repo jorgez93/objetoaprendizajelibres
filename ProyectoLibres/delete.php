a2b2bbfc961f461cf6723d3aa12edaec6c7e1803
jorgez93 <joluzaib@hotmail.com>
2018-12-15 13:44:39 -0500

    AÑADIENDO CARPETAS CSS

---
+++ ProyectoLibres/delete.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "delete_files.php";
+
+    function deleteOA($filename, $idOA, $pdo) {
+    //    $name = basename($filename,".zip");
+    //    $target = 'oa/' . $name . '/';
+    //    delete_files($target);
+    
+    //    $dirZip = 'zip/' . $filename;
+    //    unlink($dirZip);
+    
+        $sql = "DELETE FROM objetoaprendizaje WHERE idOA = :idOA";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(':idOA' => $idOA));
+    
+        $sql = "DELETE FROM comentario WHERE idOA = :idOA";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(':idOA' => $idOA));
+    }
